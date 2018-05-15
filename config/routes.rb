f025e77b5256edb46202ffaa6d328c2d83808cdd
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-15 16:49:16 +0200

    exo 1 done with poor design

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/tasks", to: "tasks#index", as: :tasks
+
+  get "tasks/new", to: "tasks#new", as: :task_new
+  post "/tasks", to: "tasks#create"
+
+
+
+  get "/task/:id", to: "tasks#show", as: :task
+
+  get "/task/:id/edit", to: "tasks#edit", as: :task_edit
+  patch "/task/:id", to: "tasks#update"
+
+  delete "/task/:id", to: "tasks#destroy", as: :task_destroy
+
 end
